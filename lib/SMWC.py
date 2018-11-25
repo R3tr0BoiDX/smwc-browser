a07969f3003f1a07ece028da16585b4bb5748d60
Adam Zwakenberg <shamist@gmail.com>
2018-11-25 15:07:23 -0500

    looping through/storing hacks

---
+++ lib/SMWC.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+import time
+from urllib.parse import urlparse, parse_qs
+
+hacklist = []
+
+def get_hack_list(browser,first = True,url = "https://www.smwcentral.net/?p=section&s=smwhacks"):
+	global hacklist
+	if first:
+		hacklist = []
+		print("Opening SMWC to check for new hacks...")
+
+	browser.get(url)
+	print("Trying "+url)
+	hackrows = browser.find_elements_by_xpath("//div[@id='list_content']//table//tr[position()>1]")
+
+	for row in hackrows:
+		firsttd = row.find_element_by_xpath(".//td[1]")
+		infolink = firsttd.find_element_by_xpath(".//a").get_attribute('href')
+		downloadlink = row.find_element_by_xpath(".//td[last()]//a").get_attribute('href')
+		title = firsttd.find_element_by_xpath(".//a").text
+		hack = {
+			"id":parse_qs(urlparse(infolink).query).get('id')[0],
+			"title": title,
+			"added": datetime.strptime(firsttd.find_element_by_xpath(".//span//time").text,"%Y-%m-%d %I:%M:%S %p").strftime('%s'),
+			"download-link": downloadlink,
+			"detail-link":infolink
+		}
+		print("Found "+title)
+		hacklist.append(hack)
+
+	nextpage = browser.find_element_by_xpath("//td[@id='menu2']//td[1]//a[last()]")
+	try:
+		imgtest = nextpage.find_element_by_xpath('.//img')
+		nextpage = nextpage.get_attribute('href')
+		print('Found another page, trying page '+parse_qs(urlparse(nextpage).query).get('n')[0])
+		return get_hack_list(browser,False,nextpage)
+	except Exception as e:
+		print("")
+
+	return {"updated":time.time(), "hack_list": hacklist}
--- start.py
+++ start.py
@@ -1,31 +1,18 @@
 import config
+from lib import SMWC
 
-from datetime import datetime
-from urllib.parse import urlparse, parse_qs
-
+import json
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 
 opts = Options()
 opts.headless = True
-assert opts.headless  # Operating in headless mode
+assert opts.headless
 browser = Chrome(options=opts)
-browser.get("https://www.smwcentral.net/?p=section&s=smwhacks")
 
-romlist = []
-
-print("Opening SMWC to check for new hacks...")
-for row in browser.find_elements_by_css_selector("#list_content tr:not(:first-child)"):
-	infolink = row.find_element_by_css_selector("td:first-child a").get_attribute('href')
-	downloadlink = row.find_element_by_css_selector("td:last-child").find_element_by_tag_name('a').get_attribute('href')
-	rom = {
-		"id":parse_qs(urlparse(infolink).query).get('id')[0],
-		"title": row.find_element_by_css_selector("td:first-child a").text,
-		"added": datetime.strptime(row.find_element_by_css_selector("td:first-child span time").text,"%Y-%m-%d %I:%M:%S %p"),
-		"download-link": downloadlink,
-		"detail-link":infolink
-	}
-	romlist.append(rom)
-print(romlist)
+romlist = SMWC.get_hack_list(browser)
+with open('cache/hacks.json', 'w') as fp:
+    json.dump(romlist, fp)
+##print(romlist)
 browser.close()
 quit()
