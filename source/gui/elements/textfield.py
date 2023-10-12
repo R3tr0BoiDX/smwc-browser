e8075ef2fc67a3ffaa7626f257dcecb49d6b1ab0
Janisch, Mirco <mirco.janisch@stud.h-da.de>
2023-10-12 23:30:12 +0200

    refined textfield...once again

--- source/gui/elements/textfield.py
+++ source/gui/elements/textfield.py
@@ -11,6 +11,9 @@ OFFSET_LEFT = 8
 
 BLINK_SPEED = 30
 
+# Define a list of allowed keycodes for a-z, 0-9, and special characters
+ALLOWED_KEYCODES = list(range(32, 127))
+
 
 class Textfield(GUIElement):
     def __init__(
@@ -25,6 +28,7 @@ class Textfield(GUIElement):
         self.text = None
         self.cursor_visible = True
         self.cursor_timer = 0
+        self.shift_pressed = False  # Flag to track if the Shift key is pressed
 
     def draw(self, anchor: Tuple[int, int], selected: bool):
         color = assets.COLOR_MAJOR_SELECTED if selected else assets.COLOR_MAJOR_NORMAL
@@ -95,15 +99,30 @@ class Textfield(GUIElement):
         return label_rect.unionall([description_rect, box_rect])
 
     def active(self, event: pygame.event.Event):
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN and event.unicode != "":
             if event.key == pygame.K_BACKSPACE:
                 if self.text is not None:
                     self.text = self.text[:-1]
-            elif event.key not in [pygame.K_RETURN]:  # todo: add more
-                if self.text is None:
-                    self.text = event.unicode
-                else:
-                    self.text += event.unicode
+            elif event.key in [pygame.K_LSHIFT, pygame.K_RSHIFT]:
+                self.shift_pressed = True
+
+            elif self.shift_pressed:
+                # Check if Shift is pressed and the key's unicode value is within the allowed keycodes
+                if ord(event.unicode) in ALLOWED_KEYCODES:
+                    self.add_char_to_text(event.unicode.upper())
+
+            elif event.unicode.isalnum() or ord(event.unicode) in ALLOWED_KEYCODES:
+                self.add_char_to_text(event.unicode)
+
+        elif event.type == pygame.KEYUP:
+            if event.key in [pygame.K_LSHIFT, pygame.K_RSHIFT]:
+                self.shift_pressed = False
+
+    def add_char_to_text(self, char: str):
+        if self.text is None:
+            self.text = char
+        else:
+            self.text += char
 
     def get_value(self) -> str:
         return self.text
