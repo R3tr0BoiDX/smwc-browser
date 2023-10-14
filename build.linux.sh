c22b72552c97c70ef5fa49a846889fe23027176e
Janisch, Mirco <mirco.janisch@stud.h-da.de>
2023-10-14 23:30:40 +0200

    added copying the media files and zipping them

--- build.linux.sh
+++ build.linux.sh
@@ -9,3 +9,15 @@ pip3 install -r requirements.txt -r requirements-dev.txt
 # Build the project
 echo "Building the project..."
 pyinstaller smwc-browser.spec
+
+# Copy the config file
+echo "Copying the config file..."
+cp config.example.json dist/
+
+# Copy the media folder
+echo "Copying the media folder..."
+cp -r media dist/
+
+# Zipping the dist folder
+echo "Zipping the dist folder..."
+zip -r dist.zip dist/
--- build.windows.ps1
+++ build.windows.ps1
@@ -10,3 +10,15 @@ pip install -r requirements.txt -r requirements-dev.txt
 # Build the project
 Write-Host "Building the project..."
 pyinstaller smwc-browser.spec
+
+# Copy the config file
+Write-Host "Copying the config file..."
+Copy-Item .\config.example.json .\dist\smwc-browser\config.example.json
+
+# Copy the media folder
+Write-Host "Copying the media folder..."
+Copy-Item .\media .\dist\smwc-browser\media -Recurse
+
+# Zipping the dist folder
+Write-Host "Zipping the dist folder..."
+Compress-Archive -Path .\dist\ -DestinationPath .\dist\smwc-browser.zip
